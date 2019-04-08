70e25bad6c12b2b0dc78c4df181f2ab1f55ea1d6
icanci01 <49305908+icanci01@users.noreply.github.com>
2019-04-09 00:01:34 +0300

    dsadsdas

--- OrderGenerator.java
+++ OrderGenerator.java
@@ -4,6 +4,7 @@ public class OrderGenerator {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Testrinfdsdas");
+        System.out.println("Giasou paniko");
     }
 
 }
