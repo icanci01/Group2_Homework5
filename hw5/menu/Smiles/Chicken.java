3668fd191e5af2659c5131b43aa03319493a090b
Panikos Christou <panikoschristou@yahoo.com>
2019-04-10 01:26:12 +0300

    en eprolava pollla minite avrio

--- hw5/menu/Smiles/Chicken.java
+++ hw5/menu/Smiles/Chicken.java
@@ -6,10 +6,10 @@ public class Chicken extends Smila {
 
 
     public Chicken(int size) {
-        super(size, timeRnadom(), "Chicken");
+        super(size, timeRandom(), "Chicken");
     }
 
-    private static int timeRnadom() {
+    private static int timeRandom() {
         int x = 15;
         x += Math.random() * 5;
         return x;
