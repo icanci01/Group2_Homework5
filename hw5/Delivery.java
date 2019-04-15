b90177464998601164e49ad39f49c0707e4c744f
Gchris12 <49322002+Gchris12@users.noreply.github.com>
2019-04-15 22:26:37 +0300

    testing

--- hw5/Delivery.java
+++ hw5/Delivery.java
@@ -1,7 +1,49 @@
 package hw5;
 
 public class Delivery {
+    private int id;
+    private int timeOfOrder;
+    private int timeOfDelivery;
+    private int timeOdDelivered;
+    private int timeDelay;
+    private int numberOfPites;
+    private int numberOfFries;
 
+    public Delivery(int id, int timeOfOrder, int timeOfDelivery, int timeOdDelivered, int numberOfPites, int numberOfFries) {
+        this.id = id;
+        this.timeOfOrder = timeOfOrder;
+        this.timeOfDelivery = timeOfDelivery;
+        this.timeOdDelivered = timeOdDelivered;
+        this.numberOfPites = this.numberOfPites;
+        this.numberOfFries = numberOfFries;
+        this.timeDelay = timeOdDelivered - timeOfDelivery;
+    }
 
-    num tordertdel(tdel-tdelreq)  (npp+npc+nps+npm)npf
+    public int getId() {
+        return id;
+    }
+
+    public int getNumberOfFries() {
+        return numberOfFries;
+    }
+
+    public int getNumberOfPites() {
+        return numberOfPites;
+    }
+
+    public int getTimeDelay() {
+        return timeDelay;
+    }
+
+    public int getTimeOdDelivered() {
+        return timeOdDelivered;
+    }
+
+    public int getTimeOfDelivery() {
+        return timeOfDelivery;
+    }
+
+    public int getTimeOfOrder() {
+        return timeOfOrder;
+    }
 }
