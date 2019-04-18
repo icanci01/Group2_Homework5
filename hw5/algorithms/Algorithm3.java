cf5b4be1d6c90e470ae4b5a0747941fe293c091c
Gchris12 <49322002+Gchris12@users.noreply.github.com>
2019-04-19 00:21:41 +0300

    Revert "Merge branch 'master' of C:\Users\userr\IdeaProjects\Group2_Homework5 with conflicts."
    This reverts commit 929d60afc80dcf7781f4105418a87467f266e4c9.

--- hw5/algorithms/Algorithm3.java
+++ hw5/algorithms/Algorithm3.java
@@ -1,14 +1,48 @@
 package hw5.algorithms;
 
 import hw5.Delivery;
+import hw5.Foukou;
 import hw5.Order;
 
 import java.util.ArrayList;
 
 public class Algorithm3 {
 
-    public static ArrayList<Delivery> useArlgorithm3(ArrayList<Order> ordersList) {
-      return null;
+    public static ArrayList<Delivery> useArlgorithm3(ArrayList<Order> ordersList, int sizeFoukou, int xronoKarvouna, int numTigania, int tiganiaCapacity) {
+        int[] texec = new int[ordersList.size()];
+        double[] w = new double[ordersList.size()];
+        Foukou foukou = new Foukou(sizeFoukou);
+        ArrayList<Delivery> deliveryList = new ArrayList<>(ordersList.size());
+
+        for (int i = 0; i < ordersList.size(); i++) {
+
+
+            for (int j = 0; j < ordersList.size(); j++) {
+                texec[j] = Foukou.timeCaclculator(ordersList.get(j), foukou);
+                w[j] = (texec[j] - (ordersList.get(j).gettRequested() - Foukou.counter)) / texec[j];
+            }
+
+            double maxW = 0;
+            int position = 0;
+            for (int k = 0; k < w.length; k++) {
+                if (w[k] > maxW) {
+                    maxW = w[k];
+                    position = k;
+                }
+            }
+
+            Order process = new Order(ordersList.get(position));
+            int tDelivered = Foukou.foukouDistribution(process, foukou);
+            deliveryList.add(new Delivery(process.getIdNum(), process.gettOrder(),
+                    process.gettRequested(), tDelivered, process.getNumberOfPites(), process.getNumberOfFries()));
+
+
+            w[position] = 0;
+
+
+        }
+        System.out.println(deliveryList.size());
+        return deliveryList;
     }
 
 }
